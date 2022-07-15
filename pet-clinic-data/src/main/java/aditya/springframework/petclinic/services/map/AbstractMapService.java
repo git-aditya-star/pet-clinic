c55926966678cafef8e9bd09af08b24bfb571ca1
git-aditya-star <adityachavan095@gmail.com>
2022-07-15 19:01:23 +0530

    Refactored Map Impl to  abstract ID property. closes #23.

--- pet-clinic-data/src/main/java/aditya/springframework/petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/aditya/springframework/petclinic/services/map/AbstractMapService.java
@@ -1,13 +1,13 @@
 package aditya.springframework.petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-//Raw Hibernate Implementation using Map
-public abstract class AbstractMapService<T, ID> {
+import aditya.springframework.petclinic.services.model.BaseEntity;
 
-    protected Map<ID, T> map  = new HashMap<>();
+import java.util.*;
+
+//Raw Hibernate Implementation using Map
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> { // BaseEntity for getId property and Long for Collections functions to take effect. Used Generics so that it will be used by all different types of Classes with same methods with different return type
+
+    protected Map<Long, T> map  = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -18,8 +18,17 @@ public abstract class AbstractMapService<T, ID> {
 
     }
 
-    T save(ID id, T object){
-        map.put(id, object);
+    T save( T object){   //if no id is assigned then assign one to the object and then insert it to the map.
+        if(object != null){
+            if(object.getId() == null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        }
+        else {
+            throw new RuntimeException("Object cannot be null");
+        }
+
 
         return object;
     }
@@ -30,5 +39,15 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object){  //entrySet is needed to iterate over map. removeIf removes if condition is true and is checked with each value of map
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId(){
+        Long nextId = null;
+        try {                                      //For map is empty which is null
+            nextId = Collections.max(map.keySet()) + 1;
+        }catch (NoSuchElementException e){
+            nextId =1L;
+        }
+        return nextId;
     }
 }
--- pet-clinic-web/src/main/java/aditya/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/aditya/springframework/petclinic/bootstrap/DataLoader.java
@@ -24,13 +24,13 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception { //spring runs this method
         //creating raw data as not yet connected to any database
         Owner owner1 = new Owner();
-        owner1.setId(1L);
+
         owner1.setFirstName("Naruto");
         owner1.setLastName("Uzumaki");
         ownerService.save(owner1);  //saving the objects to the map for id
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
+
         owner2.setFirstName("Minato");
         owner2.setLastName("Namikaze");
         ownerService.save(owner2);
@@ -38,13 +38,13 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loading Owners ---");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
+
         vet1.setFirstName("Itachi");
         vet1.setLastName("Uchiha");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
+
         vet2.setFirstName("Kakashi");
         vet2.setLastName("Hatake");
         vetService.save(vet2);
