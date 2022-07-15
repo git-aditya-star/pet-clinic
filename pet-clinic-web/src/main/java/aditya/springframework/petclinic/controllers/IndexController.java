b532d464cd619090247f29f7c2aa27d8f0e1398a
git-aditya-star <adityachavan095@gmail.com>
2022-07-15 16:53:25 +0530

    Adding Index Page and controller. Closes #8.

---
+++ pet-clinic-web/src/main/java/aditya/springframework/petclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package aditya.springframework.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index","index.html"})  //used for http request
+    public String index(){
+        return "index";
+    }
+}
