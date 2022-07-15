2130e767075f2cec9c31eefc13f1b1693ed0f775
git-aditya-star <adityachavan095@gmail.com>
2022-07-15 18:29:52 +0530

    Added Owners list and updated OwnerController.java. closes #16.

--- pet-clinic-web/src/main/java/aditya/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/aditya/springframework/petclinic/controllers/OwnerController.java
@@ -1,14 +1,23 @@
 package aditya.springframework.petclinic.controllers;
 
+import aditya.springframework.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")  //since every request has owners as start point we can use here so it automatically get added to all the subsequent request mappings
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/index","/index.html"})
-    public  String getListOwners(){
+    public  String getListOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());  //create a model with all the objects of Owner using the findAll method
         return "owners/index";
     }
 }
